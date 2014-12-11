700a1ae4294c1d52af0c6d74e6e3d3bdc4f55490
QuinnFreedman <quinnfreedman@gmail.com>
2014-12-10 20:59:34 -0800

    Create Function.java

---
+++ Function.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Function extends VObject{
+	public ArrayList<Primative.DataType> inputs;
+	public Node inputSendNode;
+	public VObject inputSend;
+	public Node inputNode;
+	public void run(){
+		
+	}
+}
